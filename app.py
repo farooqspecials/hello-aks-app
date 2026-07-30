28771447e852270cfb047c9c55a29b49b0454e94
Farooq Abdullah <farooqspecials@gmail.com>
2026-07-31 00:17:19 +0300

    Update Flask message to test 3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Farooq!-full automation using github action and azure "
+    return "Hello from Farooq!-git hub actions to azure "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
