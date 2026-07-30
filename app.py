886a1b738dde83a83a8261ec5cafb20b3d175701
Farooq Abdullah <farooqspecials@gmail.com>
2026-07-31 00:26:17 +0300

    azure automation working with github actions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Farooq!-test 4  "
+    return "Hello from Farooq!- adeel bhi sirf app k lia "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
